db9d980f53c56c9d28a02d99c019915a3312c914
Bruno Lucas <rukaso.mero@gmail.com>
2018-05-12 21:22:14 -0300

    Teste de persistência no banco.

--- src/test/java/br/com/lucas/starwarsapi/repository/PlanetaRepositoryTest.java
+++ src/test/java/br/com/lucas/starwarsapi/repository/PlanetaRepositoryTest.java
@@ -2,12 +2,15 @@ package br.com.lucas.starwarsapi.repository;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.CoreMatchers.nullValue;
 import static org.junit.Assert.assertThat;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.hamcrest.core.IsEqual;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -38,10 +41,11 @@ public class PlanetaRepositoryTest {
     public void setup() {
         DefaultMockMvcBuilder builder = MockMvcBuilders.webAppContextSetup(this.wac);
         this.mockMvc = builder.build();
+
     }
     
     @Test
-    public void deveObterPlanetaPorNome(){
+    public void deveSalvarPlaneta(){
     	
     	Planeta planeta = new Planeta();
     	planeta.setNome("Tatooine");
@@ -50,5 +54,52 @@ public class PlanetaRepositoryTest {
     	Planeta planetaSalvo = planetaRepository.save(planeta);
     	assertThat(planetaSalvo, is(notNullValue()));
     	assertThat(planetaSalvo.getId(), IsEqual.equalTo(planeta.getId()));
+    	
     }
+    
+    @Test
+    public void deveListarTodosPlanetas(){
+    	List<Planeta> planetas = planetaRepository.findAll();
+    	assertThat(planetas, is(notNullValue()));
+    }
+    
+    @Test
+    public void deveBuscarPlanetaPorNome(){
+    	
+    	salvarPlanetaParaTeste();
+    	
+    	Planeta yavin = planetaRepository.findByNome("Yavin");
+    	assertThat(yavin, is(notNullValue()));
+    	assertThat(yavin.getNome(), is("Yavin"));
+    	assertThat(yavin.getTerreno(), is("Gas"));
+    }
+
+    @Test
+    public void deveBuscarPlanetaPorId(){
+    	Planeta planeta = salvarPlanetaParaTeste();
+    	Planeta yavin = planetaRepository.findById(planeta.getId());
+    	assertThat(yavin, is(notNullValue()));
+    	assertThat(yavin.getId(), is(planeta.getId()));
+    
+    }
+    
+    @Test
+    public void deveRemoverPlaneta(){
+    	Planeta planeta = salvarPlanetaParaTeste();
+    	planetaRepository.delete(planeta);
+    	Planeta yavin = planetaRepository.findById(planeta.getId());
+    	assertThat(yavin, is(nullValue()));
+    
+    }
+    
+    public Planeta salvarPlanetaParaTeste(){
+    	Planeta planeta = new Planeta();
+    	planetaRepository.deleteByNome("Yavin");
+    	planeta.setNome("Yavin");
+    	planeta.setTerreno("Gas");
+    	planeta.setId(UUID.randomUUID());
+    	planetaRepository.save(planeta);
+    	return planeta;
+    }
+    
 }
